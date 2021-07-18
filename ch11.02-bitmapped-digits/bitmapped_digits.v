612d4e4b0be3f55af1975bfbca421bd627a7a909
Dave Dribin <dave-vcs@dribin.org>
2021-07-18 00:42:02 -0500

    Fix bug where xofs was indexing into undefined bits of bits by making bits 8 bits instead of 5

--- ch11.02-bitmapped-digits/bitmapped_digits.v
+++ ch11.02-bitmapped-digits/bitmapped_digits.v
@@ -12,80 +12,79 @@ module bitmapped_digits (
   wire [3:0] digit = i_hpos[7:4];
   wire [2:0] xofs = i_hpos[3:1];
   wire [2:0] yofs = i_vpos[3:1];
-  wire [4:0] bits;
 
-  reg [4:0] bits;	// output (5 bits)
+  reg [7:0] bits;	// output (5 bits)
   
   // combine {digit,yofs} into single ROM address
-  wire [6:0] caseexpr = {digit,yofs};
+  wire [6:0] address = {digit,yofs};
   
   always @(*)
-    case (caseexpr)/*{w:5,h:5,count:10}*/
-      7'o00: bits = 5'b11111;
-      7'o01: bits = 5'b10001;
-      7'o02: bits = 5'b10001;
-      7'o03: bits = 5'b10001;
-      7'o04: bits = 5'b11111;
+    case (address)/*{w:5,h:5,count:10}*/
+      7'o00: bits = 8'b11111;
+      7'o01: bits = 8'b10001;
+      7'o02: bits = 8'b10001;
+      7'o03: bits = 8'b10001;
+      7'o04: bits = 8'b11111;
 
-      7'o10: bits = 5'b01100;
-      7'o11: bits = 5'b00100;
-      7'o12: bits = 5'b00100;
-      7'o13: bits = 5'b00100;
-      7'o14: bits = 5'b11111;
+      7'o10: bits = 8'b01100;
+      7'o11: bits = 8'b00100;
+      7'o12: bits = 8'b00100;
+      7'o13: bits = 8'b00100;
+      7'o14: bits = 8'b11111;
 
-      7'o20: bits = 5'b11111;
-      7'o21: bits = 5'b00001;
-      7'o22: bits = 5'b11111;
-      7'o23: bits = 5'b10000;
-      7'o24: bits = 5'b11111;
+      7'o20: bits = 8'b11111;
+      7'o21: bits = 8'b00001;
+      7'o22: bits = 8'b11111;
+      7'o23: bits = 8'b10000;
+      7'o24: bits = 8'b11111;
 
-      7'o30: bits = 5'b11111;
-      7'o31: bits = 5'b00001;
-      7'o32: bits = 5'b11111;
-      7'o33: bits = 5'b00001;
-      7'o34: bits = 5'b11111;
+      7'o30: bits = 8'b11111;
+      7'o31: bits = 8'b00001;
+      7'o32: bits = 8'b11111;
+      7'o33: bits = 8'b00001;
+      7'o34: bits = 8'b11111;
 
-      7'o40: bits = 5'b10001;
-      7'o41: bits = 5'b10001;
-      7'o42: bits = 5'b11111;
-      7'o43: bits = 5'b00001;
-      7'o44: bits = 5'b00001;
+      7'o40: bits = 8'b10001;
+      7'o41: bits = 8'b10001;
+      7'o42: bits = 8'b11111;
+      7'o43: bits = 8'b00001;
+      7'o44: bits = 8'b00001;
 
-      7'o50: bits = 5'b11111;
-      7'o51: bits = 5'b10000;
-      7'o52: bits = 5'b11111;
-      7'o53: bits = 5'b00001;
-      7'o54: bits = 5'b11111;
+      7'o50: bits = 8'b11111;
+      7'o51: bits = 8'b10000;
+      7'o52: bits = 8'b11111;
+      7'o53: bits = 8'b00001;
+      7'o54: bits = 8'b11111;
 
-      7'o60: bits = 5'b11111;
-      7'o61: bits = 5'b10000;
-      7'o62: bits = 5'b11111;
-      7'o63: bits = 5'b10001;
-      7'o64: bits = 5'b11111;
+      7'o60: bits = 8'b11111;
+      7'o61: bits = 8'b10000;
+      7'o62: bits = 8'b11111;
+      7'o63: bits = 8'b10001;
+      7'o64: bits = 8'b11111;
 
-      7'o70: bits = 5'b11111;
-      7'o71: bits = 5'b00001;
-      7'o72: bits = 5'b00001;
-      7'o73: bits = 5'b00001;
-      7'o74: bits = 5'b00001;
+      7'o70: bits = 8'b11111;
+      7'o71: bits = 8'b00001;
+      7'o72: bits = 8'b00001;
+      7'o73: bits = 8'b00001;
+      7'o74: bits = 8'b00001;
 
-      7'o100: bits = 5'b11111;
-      7'o101: bits = 5'b10001;
-      7'o102: bits = 5'b11111;
-      7'o103: bits = 5'b10001;
-      7'o104: bits = 5'b11111;
+      7'o100: bits = 8'b11111;
+      7'o101: bits = 8'b10001;
+      7'o102: bits = 8'b11111;
+      7'o103: bits = 8'b10001;
+      7'o104: bits = 8'b11111;
 
-      7'o110: bits = 5'b11111;
-      7'o111: bits = 5'b10001;
-      7'o112: bits = 5'b11111;
-      7'o113: bits = 5'b00001;
-      7'o114: bits = 5'b11111;
+      7'o110: bits = 8'b11111;
+      7'o111: bits = 8'b10001;
+      7'o112: bits = 8'b11111;
+      7'o113: bits = 8'b00001;
+      7'o114: bits = 8'b11111;
 
       default: bits = 0;
     endcase
   
   wire r = i_visible && 0;
-  wire g = i_visible && bits[xofs ^ 3'b111];
+  wire g = i_visible && bits[~xofs];
   wire b = i_visible && 0;
   assign o_r = r? 8'hFF : 8'h 00;
   assign o_g = g? 8'hFF : 8'h 00;
